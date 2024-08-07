8e7aca30c704a7ba1dee370db6ceb8dca307e483
Diego Barreto <Diego@YARVIZ>
2024-08-07 15:17:51 -0500

    Se crea metodo que comprueba la funcionalidad del patron singleton

--- UserInterface/Program.cs
+++ UserInterface/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace UserInterface
+﻿using Singleton;
+
+namespace UserInterface
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            ProveSingleton();
+
+        }
+
+
+        private static void ProveSingleton()
+        {
+            // Obtiene la instancia unica de la clase.
+            ClassSingleton instanceSingleton = ClassSingleton.GetInstance();
+
+            // Ejecuta una accion x
+            instanceSingleton.GetAcction();
+
+            // Aunque se intente obtener una nueva instancia, retorna la primera que se creo.
+            ClassSingleton instanceSingleton2 = ClassSingleton.GetInstance();
+
+            // Ejecuta una accion x
+            instanceSingleton2.GetAcction();
+
+            Console.WriteLine("¿Las instancias son la misma?");
+
+            // Comparamos si las instancias son las mismas
+            if (instanceSingleton == instanceSingleton2)
+                Console.WriteLine("Son iguales.");
+            else
+                Console.WriteLine("El programador miente.");
         }
     }
 }
